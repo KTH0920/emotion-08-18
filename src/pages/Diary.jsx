f3dba7758660de7c98c276c2769f1e52483d42dc
KTH0920 <taehyun9482@gmail.com>
2025-08-21 16:36:15 +0900

    commit

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+const Dairy = () => {
+  const { id } = useParams();
+  const nav = useNavigate();
 
-const Diary = () => {
-     const {id}=useParams()
-    
   return (
-    <div>{id} Diary</div>
-  )
-}
-
-export default Diary
+    <div>
+      <Header
+        title={"yyyy-mm-dd기록"}
+        leftChild={<Button text={"<뒤로가기"} />}
+        rightChild={<Button text={"수정하기"} />}
+      />
+      <Viewer />
+    </div>
+  ); 
+};
+export default Dairy;
+ 
